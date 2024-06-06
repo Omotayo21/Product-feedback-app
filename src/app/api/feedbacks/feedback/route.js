4ccc1aaa654dae1018b6521f054c97eed2ad6a07
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2024-06-07 00:31:09 +0100

    Update route.js

--- src/app/api/feedbacks/feedback/route.js
+++ src/app/api/feedbacks/feedback/route.js
@@ -21,7 +21,14 @@ export async function GET(req) {
       })
       .exec();
 
-    return new NextResponse(JSON.stringify(feedbacks), { status: 200 });
+    //return new NextResponse(JSON.stringify(feedbacks), { status: 200 });
+  const response = new NextResponse(JSON.stringify(feedbacks), { status: 200 });
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+    response.headers.set('Surrogate-Control', 'no-store');
+
+    return response;
   } catch (error) {
     console.error("Error fetching feedbacks:", error);
     return new NextResponse(JSON.stringify({ error: error.message }), {
